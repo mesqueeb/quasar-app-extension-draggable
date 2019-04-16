1d7ae0a1e578b41eabc50165779cb0c99a97341a
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-04-16 19:27:32 +0300

    feat: review

--- src/boot/index.js
+++ src/boot/index.js
@@ -1,7 +1,7 @@
 import QDraggableRows from 'quasar-app-extension-draggable/src/components/QDraggableRows'
 import QDraggableRow from 'quasar-app-extension-draggable/src/components/QDraggableRow'
 
-export default async ({ Vue }) => {
+export default ({ Vue }) => {
   Vue.component('q-draggable-rows', QDraggableRows)
   Vue.component('q-draggable-row', QDraggableRow)
 }
